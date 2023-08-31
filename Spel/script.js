83715abbc00e72f4e0cefbc89aeb92aadbbf3428
Shadr <Leo@tictactoe.se>
2023-08-31 21:23:48 +0200

    Massor av ändringar..

--- Spel/script.js
+++ Spel/script.js
@@ -1,97 +1,122 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const cells = document.querySelectorAll('.cell');
+    const draggablePiece = document.createElement('div');
+draggablePiece.className = 'draggablePiece';
+    cells.forEach(cell => cell.addEventListener('mousedown', handleMouseDown));
+    cells.forEach(cell => cell.addEventListener('mouseup', handleMouseUp));
+    cells.forEach(cell => cell.addEventListener('mousemove', handleMouseMove));
     cells.forEach(cell => cell.addEventListener('click', handleClick));
-    cells.forEach(cell => cell.addEventListener('dragstart', handleDragStart));
-    cells.forEach(cell => cell.addEventListener('dragover', handleDragOver));
-    cells.forEach(cell => cell.addEventListener('drop', handleDrop));
 });
+
+let isDragging = false;
+let draggablePiece = document.createElement('div');
+document.body.appendChild(draggablePiece);
+draggablePiece.style.position = 'fixed';
+draggablePiece.style.opacity = '0.5';
+draggablePiece.style.width = '100px';
+draggablePiece.style.height = '100px';
+draggablePiece.style.display = 'none';
+draggablePiece.style.zIndex = '-1';  // Ensure it doesn't interfere with other elements
 
 let dragStartIndex;
 let turn = 'X';
-let xPieces = 0; // New line
-let oPieces = 0; // New line
-const board = Array(9).fill(null);
+let board = Array(9).fill(null);
+let xPieces = 0;
+let oPieces = 0;
 
-function handleDragStart(event) {
-    dragStartIndex = parseInt(event.target.id.replace('cell-', ''));
-}
-
-function handleDragOver(event) {
-    event.preventDefault();
-}
-
-function handleDrop(event) {
-    event.preventDefault();
-    const dropIndex = parseInt(event.target.id.replace('cell-', ''));
-    if (board[dropIndex] || dropIndex === dragStartIndex) {
-        return;
+function handleMouseDown(event) {
+    const index = parseInt(event.target.id.replace('cell-', ''));
+    if (board[index] === turn) {
+        isDragging = true;
+        dragStartIndex = index;
+        draggablePiece.style.backgroundImage = event.target.style.backgroundImage;
+        draggablePiece.style.display = 'block';
+        handleMouseMove(event);
     }
-    board[dropIndex] = board[dragStartIndex];
-    board[dragStartIndex] = null;
-    renderBoard();
-    turn = turn === 'X' ? 'O' : 'X';
 }
 
-function handleClick(event) {
+function handleMouseUp(event) {
+    if (isDragging) {
+        isDragging = false;
+        draggablePiece.style.display = 'none';
+
+        const dropCell = document.elementFromPoint(event.clientX, event.clientY);
+        const dropIndex = parseInt(dropCell.id.replace('cell-', ''));
+
+        if (board[dropIndex] || dropIndex === dragStartIndex) {
+            return;
+        }
+        board[dropIndex] = board[dragStartIndex];
+        board[dragStartIndex] = null;
+        renderBoard();
+        turn = turn === 'X' ? 'O' : 'X';
+    }
+}
+
+function handleMouseMove(event) {
+    if (isDragging) {
+        draggablePiece.style.left = event.clientX - 50 + 'px';
+        draggablePiece.style.top = event.clientY - 50 + 'px';
+    }
+}
+  function handleClick(event) {
     const cell = event.target;
     const index = parseInt(cell.id.replace('cell-', ''));
-    
-    if (!board[index]) {
-        if ((turn === 'X' && xPieces < 3) || (turn === 'O' && oPieces < 3)) { // New condition
-            board[index] = turn;
-
-            if (turn === 'X') {
-                xPieces++; // New line
-            } else {
-                oPieces++; // New line
-            }
-
-            if (checkWin(turn)) {
-                alert(`${turn} wins!`);
-                resetBoard();
-            }
-            turn = turn === 'X' ? 'O' : 'X';
-            renderBoard();
-        }
+    if (!board[index] && ((turn === 'X' && xPieces < 3) || (turn === 'O' && oPieces < 3))) {
+      board[index] = turn;
+      if (turn === 'X') {
+        xPieces++;
+      } else {
+        oPieces++;
+      }
+  
+      if (checkWin(turn)) {
+        alert(`${turn} wins!`);
+        setTimeout(() => resetBoard(), 5000);
+      }
+  
+      turn = turn === 'X' ? 'O' : 'X';
+      renderBoard();
     }
-}
-
-function renderBoard() {
+  }
+  
+  function renderBoard() {
     board.forEach((mark, index) => {
-        const cell = document.getElementById(`cell-${index}`);
-        if (mark === 'X') {
-            cell.style.backgroundImage = "url('https://i.imgur.com/niv24U2.png')";
-        } else if (mark === 'O') {
-            cell.style.backgroundImage = "url('https://i.imgur.com/oqjtg8K.png')";
-        } else {
-            cell.style.backgroundImage = '';
-        }
+      const cell = document.getElementById(`cell-${index}`);
+      if (mark === 'X') {
+        cell.style.backgroundImage = "url('https://i.imgur.com/niv24U2.png')";
+      } else if (mark === 'O') {
+        cell.style.backgroundImage = "url('https://i.imgur.com/oqjtg8K.png')";
+      } else {
+        cell.style.backgroundImage = '';
+      }
     });
-}
-
-function checkWin(player) {
+  }
+  
+  function checkWin(player) {
     const winningCombination = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
     ];
-    
+  
     return winningCombination.some(combination =>
-        board[combination[0]] === player &&
-        board[combination[1]] === player &&
-        board[combination[2]] === player
+      board[combination[0]] === player &&
+      board[combination[1]] === player &&
+      board[combination[2]] === player
     );
-}
-
-function resetBoard() {
+  }
+  
+  function resetBoard() {
     board.fill(null);
-    xPieces = 0; // New line
-    oPieces = 0; // New line
+    xPieces = 0;
+    oPieces = 0;
     renderBoard();
     turn = 'X';
-}
+  }
+  document.body.appendChild(draggablePiece);
